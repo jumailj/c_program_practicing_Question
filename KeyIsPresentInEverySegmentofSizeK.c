0d61185bb634ce3c36e5c7023848eeac802ff31e
jumail j <jumailjuraige@gmail.com>
2021-02-08 13:25:59 +0530

    Update KeyIsPresentInEverySegmentofSizeK.c

--- KeyIsPresentInEverySegmentofSizeK.c
+++ KeyIsPresentInEverySegmentofSizeK.c
@@ -1,19 +1,17 @@
-/* Check if a key is present in every segment of size k in an array 
-
- Given an array arr[] and size of array is n and one another key x,
+/* Check if a key is present in every segment of size k in an array */
+/* Given an array arr[] and size of array is n and one another key x,
  and give you a segment size k. The task is to find that the key x
   present in every segment of size k in arr[].
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
 
-    int arr[]  = {1,2,10,3,2,10,3,2,5,2,3,4};
+    int arr[]  = {1,10 ,10,3 ,2,10 ,10,2, 5,10 ,10,4,2};
     size_t arr_size= (sizeof(arr))/4;
     int Correct = 0;
 
-    int key = 5;
+    int key = 2;
     int x = 10;
 
     int valueFoundInEachKey = arr_size/key;
@@ -24,22 +22,15 @@ int main() {
     for(int i = 0; i < valueFoundInEachKey; i++) {
 
         for(int j = 0; j < key; j++) {
+            
             if ( arr[v] == x) {
-                *(FoundOrNot+i) = 1; 
-                break;
-            } else {
-                *(FoundOrNot+i) = 0;
-            }
+                printf("%d\t", arr[v]);
+                Correct++;
+            } 
             v++;
         }
     }
 
-    for (int i = 0; i < valueFoundInEachKey; i++) {
-
-        if ( FoundOrNot[i] == 1) {
-            Correct++;
-        }
-    }
     if ( Correct == valueFoundInEachKey ) {
         printf("yes");
     } else {
